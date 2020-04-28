84df9ead8f92f7caff321cd5aa53e1eb5f722a21
TruongGiangLe <truonggiangle17998@gmail.com>
2020-04-28 22:09:37 +0700

    read file csv

---
+++ distributed_computing/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+void read()
+{
+    ifstream fin;
+    string line;
+    // Open an existing file
+    fin.open("diabetes.csv");
+     while(!fin.eof()){
+                fin>>line;
+            cout<<line<<" "<<endl;
+            }
+    }
+int main()
+{
+    read();
+    return 0;
+}

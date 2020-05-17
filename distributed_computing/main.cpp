2e02b06dcc317b85ffaf738ac553ee3593d2a81d
TruongGiangLe <truonggiangle17998@gmail.com>
2020-05-17 11:13:39 +0700

    read file by column

--- distributed_computing/main.cpp
+++ distributed_computing/main.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include<fstream>
+#include <sstream>
+#include <vector>
+
 using namespace std;
-void read()
-{
+// for string delimiter
+vector<string> split (const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+    return result;
+}
+
+vector<double> readByColumn(int pos) {
     ifstream fin;
-    string line;
+    string line1;
+    string line2;
+    vector<double> result;
     // Open an existing file
     fin.open("diabetes.csv");
-     while(!fin.eof()){
-                fin>>line;
-            cout<<line<<" "<<endl;
-            }
+    fin>>line1;
+    while(!fin.eof()){
+       fin>>line2;
+       vector<string> temp = split(line2, ',');
+       result.push_back(atof(temp[pos-1].c_str()));
+       //atof(num.c_str())
+    }
+    return result;
     }
 int main()
 {
-    read();
+    // dọc cot nao thi truyen vao so thu tu cot do, tinh tu 1
+    vector<double> cot1 = readByColumn(7);
+    for (auto i : cot1) cout << i << endl;
     return 0;
 }

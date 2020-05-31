ea52a9dd8ef673ac3951f89340f35174eaf32331
vannn1604 <van.nn1604@gmail.com>
2020-05-31 16:09:59 +0700

    probe handle time

--- naive_bayes.cpp
+++ naive_bayes.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 
+#define NUM_THREAD 8
+
 using namespace std;
 
 /**
- * Reads csv file into table, exported as a vector of vector of doubles.
+ * Reads csv file into table, exported as a vector of vector of floats.
  * @param inputFileName input file name (full path).
- * @return data as vector of vector of doubles.
+ * @return data as vector of vector of floats.
  */
-vector<vector<double>> load_data(string inputFileName)
+vector<vector<float>> load_data(string inputFileName)
 {
 
-    vector<vector<double>> data;
+    vector<vector<float>> data;
     ifstream inputFile(inputFileName);
     int l = 0;
 
@@ -23,7 +25,7 @@ vector<vector<double>> load_data(string inputFileName)
         if (s[0] != '#')
         {
             istringstream ss(s);
-            vector<double> record;
+            vector<float> record;
 
             while (ss)
             {
@@ -41,7 +43,8 @@ vector<vector<double>> load_data(string inputFileName)
                     e.what();
                 }
             }
-
+            if (record.back() != 1)
+                record.at(record.size() - 1) = 0;
             data.push_back(record);
         }
     }
@@ -55,11 +58,11 @@ vector<vector<double>> load_data(string inputFileName)
     return data;
 }
 
-void zip(vector<vector<double>> &b)
+void zip(vector<vector<float>> &b)
 {
     if (b.size() == 0)
         return;
-    vector<vector<double>> trans_vec(b[0].size(), vector<double>());
+    vector<vector<float>> trans_vec(b[0].size(), vector<float>());
     for (int i = 0; i < b.size(); i++)
     {
         for (int j = 0; j < b[i].size(); j++)
@@ -71,27 +74,27 @@ void zip(vector<vector<double>> &b)
 }
 
 // tinh toan gia tri trung binh cua moi thuoc tinh
-double mean(vector<double> numbers)
+float mean(vector<float> numbers)
 {
-    return accumulate(numbers.begin(), numbers.end(), 0) / double(numbers.size());
+    return accumulate(numbers.begin(), numbers.end(), 0) / float(numbers.size());
 }
 
 // Tinh toan do lech chuan cho tung thuoc tinh
-double standard_deviation(vector<double> numbers)
+float standard_deviation(vector<float> numbers)
 {
-    double avg = mean(numbers);
-    vector<double> a;
+    float avg = mean(numbers);
+    vector<float> a;
     for (unsigned i = 0; i < numbers.size(); i++)
         a.push_back(pow(numbers.at(i) - avg, 2));
-    double variance = accumulate(a.begin(), a.end(), 0) / double(numbers.size() - 1);
+    float variance = accumulate(a.begin(), a.end(), 0) / float(numbers.size() - 1);
     return sqrt(variance);
 }
 
 // Gia tri trung binh , do lech chuan
-vector<tuple<double, double>> summarize(vector<vector<double>> dataset)
+vector<tuple<float, float>> summarize(vector<vector<float>> dataset)
 {
     zip(dataset);
-    vector<tuple<double, double>> summaries;
+    vector<tuple<float, float>> summaries;
     for (unsigned i = 0; i < dataset.size(); i++)
     {
         auto tup = make_tuple(mean(dataset.at(i)), standard_deviation(dataset.at(i)));
@@ -102,59 +105,61 @@ vector<tuple<double, double>> summarize(vector<vector<double>> dataset)
 }
 
 // Phan chia tap du lieu theo class
-map<double, vector<vector<double>>> separate_data(vector<vector<double>> dataset)
+map<float, vector<vector<float>>> separate_data(vector<vector<float>> dataset)
 {
-    map<double, vector<vector<double>>> separated;
+    map<float, vector<vector<float>>> separated;
+    vector<float> vec;
     for (int i = 0; i < dataset.size(); i++)
     {
-        vector<double> vec = dataset.at(i);
+        vec = dataset.at(i);
         if (!separated.count(vec.back()))
-            separated[vec.back()] = vector<vector<double>>();
+            separated[vec.back()] = vector<vector<float>>();
         separated[vec.back()].push_back(vec);
     }
     return separated;
 }
 
-map<double, vector<tuple<double, double>>> summarize_by_class(vector<vector<double>> dataset)
+map<float, vector<tuple<float, float>>> summarize_by_class(vector<vector<float>> dataset)
 {
-    map<double, vector<vector<double>>> separated = separate_data(dataset);
-    map<double, vector<tuple<double, double>>> summaries;
-    for (map<double, vector<vector<double>>>::iterator it = separated.begin(); it != separated.end(); ++it)
+    map<float, vector<vector<float>>> separated = separate_data(dataset);
+    map<float, vector<tuple<float, float>>> summaries;
+    for (map<float, vector<vector<float>>>::iterator it = separated.begin(); it != separated.end(); ++it)
         summaries[it->first] = summarize(it->second);
 
     return summaries;
 }
 
-double calculate_prob(double x, double mean, double stdev)
+float calculate_prob(float x, float mean, float stdev)
 {
     float exponent = exp(-(pow(x - mean, 2) / (2 * pow(stdev, 2))));
     return (1 / (sqrt(2 * M_PI) * stdev)) * exponent;
 }
 
 // Tinh xac suat cho moi thuoc tinh phan chia theo class
-map<double, double> calculate_class_prob(map<double, vector<tuple<double, double>>> summaries, vector<double> input_vector)
+map<float, float> calculate_class_prob(map<float, vector<tuple<float, float>>> summaries, vector<float> input_vector)
 {
-    map<double, double> probabilities;
-    for (map<double, vector<tuple<double, double>>>::iterator it = summaries.begin(); it != summaries.end(); ++it)
+    map<float, float> probabilities;
+    for (map<float, vector<tuple<float, float>>>::iterator it = summaries.begin(); it != summaries.end(); ++it)
     {
         probabilities[it->first] = 1;
         for (int i = 0; i < (it->second).size(); ++i)
         {
-            double mean = get<0>((it->second).at(i));
-            double stdev = get<1>((it->second).at(i));
-            double x = input_vector[i];
-            probabilities.at(it->first) *= calculate_prob(x, mean, stdev);
+            float mean = get<0>((it->second).at(i));
+            float stdev = get<1>((it->second).at(i));
+            float x = input_vector[i];
+            float a = calculate_prob(x, mean, stdev);
+            probabilities.at(it->first) *= a;
         }
     }
     return probabilities;
 }
 
 // Du doan vector thuoc phan lop nao
-double predict(map<double, vector<tuple<double, double>>> summaries, vector<double> input_vector)
+float predict(map<float, vector<tuple<float, float>>> summaries, vector<float> input_vector)
 {
-    map<double, double> probabilities = calculate_class_prob(summaries, input_vector);
-    double best_label = 9999, best_prob = -1;
-    for (map<double, double>::iterator it = probabilities.begin(); it != probabilities.end(); ++it)
+    map<float, float> probabilities = calculate_class_prob(summaries, input_vector);
+    float best_label = 9999, best_prob = -1;
+    for (map<float, float>::iterator it = probabilities.begin(); it != probabilities.end(); ++it)
         if (best_label == 9999 || it->second > best_prob)
         {
             best_prob = it->second; //probability
@@ -164,43 +169,76 @@ double predict(map<double, vector<tuple<double, double>>> summaries, vector<doub
 }
 
 // Du doan tap du lieu testing thuoc vao phan lop nao
-vector<double> get_predictions(map<double, vector<tuple<double, double>>> summaries, vector<vector<double>> test_set)
+vector<float> get_predictions(map<float, vector<tuple<float, float>>> summaries, vector<vector<float>> test_set)
 {
-    vector<double> predictions;
+    vector<float> predictions;
+    #pragma omp parallel for num_threads(NUM_THREAD)
     for (int i = 0; i < test_set.size(); i++)
         predictions.push_back(predict(summaries, test_set[i]));
     return predictions;
 }
 
 // Tinh toan do chinh xac cua phan lop
-double get_accuracy(vector<vector<double>> test_set, vector<double> predictions)
+float get_accuracy(vector<vector<float>> test_set, vector<float> predictions)
 {
-    double correct = 0;
+    float correct = 0;
     for (int i = 0; i < test_set.size(); ++i)
         if (test_set.at(i).back() == predictions.at(i))
             correct += 1;
-    return (correct / double(test_set.size())) * 100.0;
+    return (correct / float(test_set.size())) * 100.0;
 }
 
 int main()
 {
-    double split_ratio = 0.8;
-    vector<vector<double>> dataset = load_data("diabetes.csv");
+    auto start = chrono::system_clock::now();
+    float split_ratio = 0.8;
+    vector<vector<float>> dataset = load_data("data.csv");
+    auto end = chrono::system_clock::now();
+    chrono::duration<float> elapsed_seconds = end - start;
+    cout << "Time load data: " << elapsed_seconds.count() << "\n";
+
+
     // chia train(train_set), test(copy)
-    double train_size = dataset.size() * split_ratio;
-    vector<vector<double>> train_set;
-    vector<vector<double>> test_set = dataset;
-    srand((int) time(0));
+    float train_size = int(dataset.size() * split_ratio);
+    vector<vector<float>> train_set;
+    vector<vector<float>> test_set = dataset;
+
+
+    auto start_ = chrono::system_clock::now();
+    srand((int)time(0));
     while (train_set.size() < train_size)
     {
         int index = rand() % test_set.size();
         train_set.push_back(test_set.at(index));
         test_set.erase(test_set.begin() + index);
     }
+    
+    end = chrono::system_clock::now();
+    elapsed_seconds = end - start_;
+    cout << "Time chia train test: " << elapsed_seconds.count() << "\n";
     //
-    map<double, vector<tuple<double, double>>> summaries = summarize_by_class(train_set);
-    vector<double> predictions = get_predictions(summaries, test_set);
-    double accuracy = get_accuracy(test_set, predictions);
+
+
+    start_ = chrono::system_clock::now();
+    map<float, vector<tuple<float, float>>> summaries = summarize_by_class(train_set);
+    end = chrono::system_clock::now();
+    elapsed_seconds = end - start_;
+    cout << "Time summarize_by_class: " << elapsed_seconds.count() << "\n";
+
+    vector<float> predictions = get_predictions(summaries, test_set);
+    float accuracy = get_accuracy(test_set, predictions);
+
+    vector<vector<float>> data = load_data("benhnhan1.csv");
+    vector<float> prediction = get_predictions(summaries, data);
+
+    end = chrono::system_clock::now();
+    elapsed_seconds = end - start;
+    time_t end_time = std::chrono::system_clock::to_time_t(end);
     cout << "Accuracy of my implement: " << accuracy << "%\n";
+    if (prediction.at(0) == 0)
+        cout << "Khong mac benh\n";
+    else
+        cout << "Mac benh\n";
+    cout << "Time handle: " << elapsed_seconds.count() << "\n";
     return 0;
 }
